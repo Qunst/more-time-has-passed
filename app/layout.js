736badedbacc13357d7901f9d9d585986ec2393a
Qunst <davorinkunst.1@gmail.com>
2026-03-22 11:52:02 +0100

    Add default Open Graph image

--- app/layout.js
+++ app/layout.js
@@ -24,13 +24,22 @@ export const metadata = {
     url: 'https://moretimehaspassed.com',
     siteName: 'More Time Has Passed',
     locale: 'en_US',
-    type: 'website'
+    type: 'website',
+    images: [
+      {
+        url: '/og-default.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'More Time Has Passed'
+      }
+    ]
   },
   twitter: {
     card: 'summary_large_image',
     title: 'More Time Has Passed',
     description:
-      'Surprising time comparisons from movies, TV, music, tech, gaming, history, and internet culture.'
+      'Surprising time comparisons from movies, TV, music, tech, gaming, history, and internet culture.',
+    images: ['/og-default.jpg']
   }
 };
 
