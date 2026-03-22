36ce5163b16c951ed8b2a114e14db31dbbafcf69
Qunst <davorinkunst.1@gmail.com>
2026-03-22 12:06:20 +0100

    Add explicit OG image to fact page metadata

--- app/facts/[slug]/page.js
+++ app/facts/[slug]/page.js
@@ -25,12 +25,21 @@ export async function generateMetadata({ params }) {
       description,
       url,
       siteName: 'More Time Has Passed',
-      type: 'article'
+      type: 'article',
+      images: [
+        {
+          url: '/og-default.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'More Time Has Passed'
+        }
+      ]
     },
     twitter: {
       card: 'summary_large_image',
       title,
-      description
+      description,
+      images: ['/og-default.jpg']
     }
   };
 }
